73b6e88266382ac32aeadfaf89508dde7d82b9d0
Rodolphe S. Eugene <92120435+RodolpheEugene@users.noreply.github.com>
2023-03-05 03:23:57 -0500

    Finished PartB

--- MiniProject1/PartB/PartB.cpp
+++ MiniProject1/PartB/PartB.cpp
@@ -10,54 +10,64 @@ using std::pair;
 using std::make_pair;
 
 class HeapBuilder {
- private:
-  vector<int> data_;
-  vector< pair<int, int> > swaps_;
+private:
+    vector<int> data_;
+    vector< pair<int, int> > swaps_;
 
-  void WriteResponse() const {
-    cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
-      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
-    }
-  }
-
-  void ReadData() {
-    int n;
-    cin >> n;
-    data_.resize(n);
-    for(int i = 0; i < n; ++i)
-      cin >> data_[i];
-  }
-
-  void GenerateSwaps() {
-    swaps_.clear();
-    // The following naive implementation just sorts 
-    // the given sequence using selection sort algorithm
-    // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
-    // but in the worst case gives a quadratic number of swaps.
-    //
-    // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
+    void WriteResponse() const {
+        cout << swaps_.size() << "\n";
+        for (int i = 0; i < swaps_.size(); ++i) {
+            cout << swaps_[i].first << " " << swaps_[i].second << "\n";
         }
-      }
-  }
+    }
 
- public:
-  void Solve() {
-    ReadData();
-    GenerateSwaps();
-    WriteResponse();
-  }
+    void ReadData() {
+        int n;
+        cin >> n;
+        data_.resize(n);
+        for (int i = 0; i < n; ++i)
+            cin >> data_[i];
+    }
+
+    void GenerateSwaps() {
+        swaps_.clear();
+        // Starting from the middle of the array, we iterate backwards 
+        // and sift down each element to its correct position in the heap
+        for (int i = data_.size() / 2; i >= 0; --i) {
+            SiftDown(i);
+        }
+    }
+
+    void SiftDown(int i) {
+        int minIndex = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < data_.size() && data_[leftChild] < data_[minIndex]) {
+            minIndex = leftChild;
+        }
+        if (rightChild < data_.size() && data_[rightChild] < data_[minIndex]) {
+            minIndex = rightChild;
+        }
+        if (i != minIndex) {
+            swap(data_[i], data_[minIndex]);
+            swaps_.push_back(make_pair(i, minIndex));
+            SiftDown(minIndex);
+        }
+    }
+
+
+public:
+    void Solve() {
+        ReadData();
+        GenerateSwaps();
+        WriteResponse();
+    }
 };
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  HeapBuilder heap_builder;
-  heap_builder.Solve();
-  return 0;
+    std::ios_base::sync_with_stdio(false);
+    HeapBuilder heap_builder;
+    heap_builder.Solve();
+    return 0;
 }
